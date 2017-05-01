494f4e5f55a0281fbfb0bca53b85443fbf7ffc95
Kimberly Horan <klhoran@ymail.com>
2017-05-01 15:08:01 -0700

    set up add blog template

--- main.py
+++ main.py
@@ -15,10 +15,29 @@
 # limitations under the License.
 #
 import webapp2
+import os
+import jinja2
 
-class MainHandler(webapp2.RequestHandler):
+template_dir = os.path.join(os.path.dirname(__file__), "templates")
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
+
+
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+
+class MainHandler(Handler):
     def get(self):
-        self.response.write('Hello world!')
+        self.render("add_blog.html")
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
